6a432dcff6dc7123357facdf4c29d12a689c08b3
nirunj27 <68642682+nirunj27@users.noreply.github.com>
2022-06-08 08:13:37 +0530

    Update Jokes.jsx

--- src/Pages/jokes/Jokes.jsx
+++ src/Pages/jokes/Jokes.jsx
@@ -1,8 +1,22 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 import { getJokes } from "./features/jokeSlice";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+});
 
 const Jokes = () => {
   const { jokes, loading } = useSelector((state) => state.joke);
@@ -10,36 +24,77 @@ const Jokes = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const classes = useStyles();
+
+  const paperStyle = {
+    padding: 20,
+    height: "auto",
+    width: 800,
+    margin: "30px auto",
+  };
+
   const loaderstyle = {
     display: "flex",
     justifyContent: "center",
-    marginTop:"200px"
+    marginTop: "200px",
   };
 
+  const tableheadStyle = {
+    backgroundColor: "#239191",
+    color: "#fff",
+    fontSize: "15px",
+    fontWeight: "600",
+  };
+
+  const headerstyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: "20px",
+  };
+
+  
   useEffect(() => {
     if (!localStorage.getItem("credauth")) {
       navigate("/login");
     } else {
       dispatch(getJokes());
     }
-  }, []);
+  }, [dispatch]);
 
   return (
-    <div>
-      {loading ? (
-        <h1>Fetch data</h1>
-      ) : (
-        <div style={loaderstyle}>
-          <CircularProgress color="secondary" />
-        </div>
-      )}
-      {/* {jokes.map((item)=>{
-         return (
-          <h2>{item.jokes.category}</h2>
-         )
-         
-       })} */}
-    </div>
+    <Paper elevation={10} style={paperStyle}>
+      <div style={headerstyle}>
+        <h3>View Jokes</h3>
+      </div>
+
+      <TableContainer>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead style={tableheadStyle}>
+            <TableRow align="center">
+              <TableCell>Category</TableCell>
+              <TableCell>Jokes</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!loading ? (
+              <TableRow align="center">
+                <TableCell component="th" scope="row"  style={{fontFamily:"'Bangers', cursive"}}>
+                  Category
+                </TableCell>
+                <TableCell  style={{fontFamily:"'Bangers', cursive"}}>
+                  Jokes
+                </TableCell>
+              </TableRow>
+            ) : (
+              <div style={loaderstyle}>
+                <CircularProgress color="secondary" />
+              </div>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 };
 
